301681e516b2b779940ee03468576d014b088df5
itsfarhankhan28 <famannat@gmail.com>
2023-06-07 13:41:33 +0530

    feat(page): added Link for users page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
-    <div className='h-screen flex items-center justify-center'>
+    <div className='h-screen flex items-center justify-center flex-col'>
       <h1 className='text-3xl font-semibold'>Homepage</h1>
+      <Link href='/users'>Go to users page</Link>
     </div>
-    </>
+    </> 
   )
 }
